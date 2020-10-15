ab956cbef616d02be2b2ebfd41674c2058225a30
oo7items <oo7items@gmail.com>
2020-10-15 20:39:39 +0800

    Updates

---
+++ precache-manifest.dc330b19f195f3899b60428350aafbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c92a7bd1ff7abe21d54aa608e2a7a9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b9b387fd57be753ff5e4",
+    "url": "/monsters-rolodex/static/css/main.a24b1c81.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b387fd57be753ff5e4",
+    "url": "/monsters-rolodex/static/js/main.a10680f9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
